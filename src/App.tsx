b779f13d4a16bee388578e997f37c9bbd56e9c27
Nikhil Singh <nkumarsingh625@gmail.com>
2026-02-12 13:41:51 +0530

    fix: switch to HashRouter and build before deploy for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Experience from './components/Experience'
 import Hero from './components/Hero'
 import SocialLinks from './components/SocialLinks'
